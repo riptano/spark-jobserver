dca191b16664e339ec7c0ec95296ab6b34f9d0d6
Russell Spitzer <Russell.Spitzer@gmail.com>
2015-06-11 18:58:11 -0700

    DSP-5892 Dse Configuration and Submit for JobServer
    We use DSE spark-submit instead of spark-submit and all of the
    environement variables which are normally set by DSE are removed from
    the base config.
    
    The stop script is modified so that it will kill the entire process
    group rather than just the DSE shell process.

--- bin/server_start.sh
+++ bin/server_start.sh
@@ -43,7 +43,7 @@ if [ -f "$PIDFILE" ] && kill -0 $(cat "$PIDFILE"); then
    exit 1
 fi
 
-cmd='$SPARK_HOME/bin/spark-submit --class $MAIN --driver-memory $JOBSERVER_MEMORY
+cmd='$DSE_HOME/bin/spark-submit --class $MAIN --driver-memory $JOBSERVER_MEMORY
   --conf "spark.executor.extraJavaOptions=$LOGGING_OPTS"
   --driver-java-options "$GC_OPTS $JAVA_OPTS $LOGGING_OPTS $CONFIG_OVERRIDES"
   $@ $appdir/spark-job-server.jar $conffile'
