faae1445e3f81e48156fa8c24f7dfb6f090fdabb
Jaroslaw Grabowski <jaroslaw.grabowski@datastax.com>
2018-01-02 10:49:32 +0100

    DSP-15163 use dse script to start context in separate jvm
    Dse script is used for manager-start-cmd because it is easily reachable
    by spark jobserver. Original value for manager-start-cmd is
    ./manager_start.sh which ends in file not found error. Providing full
    path to config file would introduce unnecessary complexity.
    
    Context-per-jvm share logging configuration with spark jobserver
    instance. However logging dir is different, every context-per-jvm
    instance have it's own logging directory.

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -43,8 +43,10 @@ if [ -z "$LOG_DIR" ]; then
 fi
 mkdir -p $LOG_DIR
 
-LOGGING_OPTS="-Dlogback.configurationFile=file:$appdir/logback-server.xml
-              -DLOG_DIR=$LOG_DIR"
+# used in server_start and in manager_start
+LOGGING_OPTS_FILE="-Dlogback.configurationFile=file:$appdir/logback-server.xml"
+
+LOGGING_OPTS="$LOGGING_OPTS_FILE -DLOG_DIR=$LOG_DIR"
 
 # For Mesos
 CONFIG_OVERRIDES="-Dspark.executor.uri=$SPARK_EXECUTOR_URI "
