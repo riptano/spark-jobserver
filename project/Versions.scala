e51cff0d5b90b52f6742b0e5cba5672234c7419e
Jaroslaw <jaroslaw.grabowski@datastax.com>
2019-08-01 16:28:20 +0200

    DSP-19490 bump spark version
    All previous spark versions were removed from our local-release repo.
    2.2.0.17 is an artificial release (no changes introduced) created just
    as a dependency of Spark Jobserver

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 import scala.util.Properties.isJavaAtLeast
 
 object Versions {
-  lazy val spark = sys.env.getOrElse("SPARK_VERSION", "2.2.0.6")
+  lazy val spark = sys.env.getOrElse("SPARK_VERSION", "2.2.3.4")
 
   lazy val akka = "2.4.9"
   lazy val dseDriver = "1.6.2"
