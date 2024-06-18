9ae0758b34ba98215bb501d77a644d55b7982892
Tiago Alves <tiago.alves@datastax.com>
2024-06-18 15:51:02 +0100

    Revert "DSP-24286 Removes Java 11 deprecated JVM options"
    This reverts commit 1f2b8c06840ff8ad6c910e00be37f4fa0c63b7a3 because it
    removes JVM settings that are valid in Java 8 and failure in Java 11
    can be simple fixed by adding `-XX:+IgnoreUnrecognizedVMOptions`.

--- bin/manager_start.sh
+++ bin/manager_start.sh
@@ -17,7 +17,9 @@ get_abs_script_path
 LOGGING_OPTS="$LOGGING_OPTS_FILE
               -DLOG_DIR=$5"
 
-GC_OPTS="-verbose:gc
+GC_OPTS="-XX:+UseConcMarkSweepGC
+         -verbose:gc -XX:+PrintGCTimeStamps
+         -XX:MaxPermSize=512m
          -XX:+CMSClassUnloadingEnabled "
 
 JAVA_OPTS="-XX:MaxDirectMemorySize=$MAX_DIRECT_MEMORY
@@ -46,7 +48,7 @@ elif [ $2 == "cluster" ]; then
 else
   JAR_FILE="$appdir/spark-job-server.jar"
   CONF_FILE="$conffile"
-  GC_OPTS="$GC_OPTS -Xlog:gc:$5/gc.out"
+  GC_OPTS="$GC_OPTS -Xloggc:$5/gc.out"
 fi
 
 if [ -n "$6" ]; then
